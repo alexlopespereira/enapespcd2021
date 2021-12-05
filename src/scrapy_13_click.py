d665571830c0e26a656d638e315022387aa9616f
Alex Lopes Pereira <alexlopespereira@gmail.com>
2021-12-05 17:21:42 -0300

    added

---
+++ src/scrapy_13_click.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+url = 'https://www.camara.leg.br/buscaProposicoesWeb/pesquisaSimplificada'
+driver.get(url)
+driver.find_element(By.XPATH,'//input[@type="checkbox"][1]').click()
+driver.find_element(By.XPATH,'//input[@id="pesquisar"]').click()
